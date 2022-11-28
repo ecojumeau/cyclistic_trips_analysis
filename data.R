2f22de9fda07241f409cd5cad5269cd9e47700ee
Edgar Contreras <contrerasedgarmichell@gmail.com>
2022-11-28 12:06:24 -0500

    Creé solo dataset a partir de toda la información, borré columnas innecesarias y confirmé el tipo de usuario

--- data.R
+++ data.R
@@ -1,4 +1,4 @@
-#Cargando librerias y paquetes
+#Cargando librerias y paquetes 
 install.packages("tidyverse")
 library(tidyverse)
 library(lubridate)
@@ -9,7 +9,7 @@ library(tidyr)
 getwd()
 setwd("C:/Users/contr/OneDrive/Documentos")
 
-#Cargando los datasets
+#Cargando los datasets desde el computador
 nov_2021 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202111-divvy-tripdata.csv")
 dec_2021 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202112-divvy-tripdata.csv")
 jan_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202201-divvy-tripdata.csv")
@@ -39,3 +39,15 @@ colnames(oct_2022)
 
 #Estos son todos los datos para el análisis.
 #A partir de ahora, se deben procesar los datos para preparar el análisis.
+
+#Combinando todos los datasets en uno solo para hacerlo más fácil de manejar.
+all_trips <- bind_rows(nov_2021,dec_2021,jan_2022,feb_2022,mar_2022,
+                       apr_2022,may_2022,jun_2022,jul_2022,aug_2022,
+                       sep_2022,oct_2022)
+
+#Borrando datos innecesarios de columnas que no usaré para el análisis.
+all_trips <- all_trips %>%
+  select(-c(start_lat,start_lng,end_lat,end_lng))
+
+#Confirmamos que solo hayan dos tipos de usuarios.
+table(all_trips$member_casual)
