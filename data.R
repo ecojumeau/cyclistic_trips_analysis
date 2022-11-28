57af38be63fb26dfb0e9dc9862ab355846b4bed0
Edgar Contreras <contrerasedgarmichell@gmail.com>
2022-11-28 02:27:20 -0500

    Cargando datos asignados a variables

--- data.R
+++ data.R
@@ -8,3 +8,18 @@ library(data.table)
 library(tidyr)
 getwd()
 setwd("C:/Users/contr/OneDrive/Documentos")
+
+#Cargando los datasets
+nov_2021 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202111-divvy-tripdata.csv")
+dec_2021 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202112-divvy-tripdata.csv")
+jan_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202201-divvy-tripdata.csv")
+feb_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202202-divvy-tripdata.csv")
+mar_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202203-divvy-tripdata.csv")
+apr_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202204-divvy-tripdata.csv")
+may_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202205-divvy-tripdata.csv")
+jun_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202206-divvy-tripdata.csv")
+jul_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202207-divvy-tripdata.csv")
+aug_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202208-divvy-tripdata.csv")
+sep_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202209-divvy-tripdata.csv")
+oct_2022 <- read.csv("C:/Users/contr/OneDrive/Documentos/Cyclistic Trips/202210-divvy-tripdata.csv")
+
